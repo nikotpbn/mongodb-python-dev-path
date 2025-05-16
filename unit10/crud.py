3fbb70a48f8387076598a4a52a9a16564fad325a
nikotpbn <niko@Nicolaus-MacBook-Pro.local>
2025-05-16 10:37:23 +0100

    updating and deleting documents

--- unit10/crud.py
+++ unit10/crud.py
@@ -3,9 +3,10 @@ from bson.objectid import ObjectId
 
 from .data.dataset import new_document, new_account, new_accounts
 
+
 def metadata(cursor):
     result = [i for i in cursor]
-    return { "num_docs": len(result), "data": result}
+    return {"num_docs": len(result), "data": result}
 
 
 def unit10(client):
@@ -13,18 +14,55 @@ def unit10(client):
     db = client.bank
     accounts_collection = db.accounts
 
-    # result = accounts_collection.insert_one(new_account)
+    result = accounts_collection.insert_one(new_account)
 
-    # document_id = result.inserted_id
-    # print(f"_id of inserted document {document_id}")
+    document_id = result.inserted_id
+    print(f"_id of inserted document {document_id}")
 
-    # result = accounts_collection.insert_many(new_accounts)
-    # document_ids = result.inserted_ids
-    # print("# of documents inserted: " + str(len(document_ids)))
-    # print(f"_ids of inserted documents: {document_ids}")
+    result = accounts_collection.insert_many(new_accounts)
+    document_ids = result.inserted_ids
+    print("# of documents inserted: " + str(len(document_ids)))
+    print(f"_ids of inserted documents: {document_ids}")
 
     result = accounts_collection.find_one({"_id": ObjectId("6825f06998552c2e961fb3aa")})
     pprint.pp(result)
 
-    cursor = accounts_collection.find({"balance": {"$gt": 4700}})
-    pprint.pp(metadata(cursor))
+    result = accounts_collection.find({"balance": {"$gt": 4700}})
+    pprint.pp(metadata(result))
+
+    result = accounts_collection.update_one(
+        {"_id": ObjectId("6826fd694315a5a3d5e42814")}, {"$inc": {"balance": 100}}
+    )
+    print("Documents updated: " + str(result.modified_count))
+    pprint.pp(result)
+
+    result = accounts_collection.update_many( {"account_type": "savings"}, {"$set": {"minimum_balance": 100}})
+    print("Documents matched: " + str(result.matched_count))
+    print("Documents modified: " + str(result.modified_count))
+    pprint.pp(accounts_collection.find_one({"account_type": "savings"}))
+
+    print("Searching for target document before delete:")
+    pprint.pp(
+        accounts_collection.find_one({"_id": ObjectId("6825f06998552c2e961fb3aa")})
+    )
+    result = accounts_collection.delete_one(
+        {"_id": ObjectId("6825f06998552c2e961fb3aa")}
+    )
+    print("Searching for target document after delete:")
+    pprint.pp(
+        accounts_collection.find_one({"_id": ObjectId("6825f06998552c2e961fb3aa")})
+    )
+    print("documents deleted: " + str(result.deleted_count))
+
+
+    print("Searching for target document before delete:")
+    pprint.pp(
+        accounts_collection.find_one({"balance": {"$lt": 2000}})
+    )
+    result = accounts_collection.delete_many({"balance": {"$lt": 2000}})
+    print("Searching for target document after delete:")
+    pprint.pp(
+        accounts_collection.find_one({"balance": {"$lt": 2000}})
+    )
+    print("documents deleted: " + str(result.deleted_count))
+
--- unit10/data/dataset.py
+++ unit10/data/dataset.py
@@ -1,4 +1,5 @@
 from bson.objectid import ObjectId
+from bson.decimal128 import Decimal128
 
 import datetime
 
@@ -45,5 +46,58 @@ new_accounts = [
         "account_type": "savings",
         "balance": 267914296,
         "last_updated": datetime.datetime.now(),
+    },
+    {
+        "_id": ObjectId("6826fd694315a5a3d5e42814"),
+        "account_holder": "Kate Stone",
+        "account_id": "MDB333829449",
+        "account_type": "checking",
+        "balance": Decimal128("4688"),
+        "transfers_complete": [
+            "TR854412948",
+            "TR413308451",
+            "TR328708274",
+            "TR192714918",
+            "TR263422717",
+        ],
+        "last_updated": datetime.datetime.now(),
+    },
+    {
+        "account_id": "MDB156014571",
+        "account_holder": "Adelen Værnes",
+        "account_type": "savings",
+        "balance": 1519.62,
+        "transfers_complete": [
+            "TR670287839",
+            "TR679752211",
+            "TR854525844",
+            "TR762109284",
+        ],
+    },
+    {
+        "account_id": "MDB190468049",
+        "account_holder": "Louis Lewis",
+        "account_type": "savings",
+        "balance": 4155.67,
+        "transfers_complete": [
+            "TR859060098",
+            "TR729044189",
+            "TR126484922",
+            "TR617907396",
+            "TR598541455",
+        ],
+    },
+    {
+        "account_id": "MDB870205338",
+        "account_holder": "Juan Perez",
+        "account_type": "checking",
+        "balance": 1907.8,
+        "transfers_complete": [
+            "TR432759196",
+            "TR797654953",
+            "TR391563093",
+            "TR464853424",
+            "TR922604241",
+        ],
     },
 ]
